1c2c04f15083a910beb33ecf78e1848b9253a73f
iitiansuyash <suyashsuryanshi11111@gmail.com>
2022-06-14 17:08:15 +0530

    changes

--- components/TypeWrite.js
+++ components/TypeWrite.js
@@ -4,12 +4,12 @@ const TypeWrite = () => {
     return ( 
       <div className={styles.rwWrapper}>
       <div className={`${styles.rwWords} ${styles.rwWords1}`}>
-        <p>Welcome To ToWaSo.</p>
-        <p>Be Clean.</p>
-        <p>Be Green.</p>
-        <p>Be A Recycling Machine.</p>
-        <p>Think Before You Trash It.</p>
-        <p>Don't Be A Punk! Recycle Yout Junk.</p>
+        <p>Welcome To ToWaSo</p>
+        <p>Be Clean</p>
+        <p>Be Green</p>
+        <p>Be A Recycling Machine</p>
+        <p>Think Before You Trash It</p>
+        <p>Don't Be A Punk! Recycle Yout Junk</p>
       </div>
     </div>
      );

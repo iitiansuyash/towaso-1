260dd28e555c1904fbd100b7945a47ce19734b71
iitiansuyash <suyashsuryanshi11111@gmail.com>
2022-06-16 02:31:23 +0530

    test Deploy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   images: {
     domains: ['via.placeholder.com', 'images.pexels.com', 'images2.alphacoders.com', 'images.unsplash.com'],
   },
